5e2dd13cbdd3b077cb48d437b4a398c264825325
Chaitu32 <sai.chaitanya@students.iiit.ac.in>
2021-03-10 01:12:01 +0530

    Genetic Code completed...:)

--- MDL-Project/client.py
+++ MDL-Project/client.py
@@ -1,12 +1,20 @@
 import json
 import requests
 import numpy as np
-from random import sample
+from random import sample,uniform
 
 API_ENDPOINT = 'http://10.4.21.156'
 MAX_DEG = 11
+MAX_POPULATION =16
 B10_V = np.zeros((10,MAX_DEG))
 B10_E = np.zeros((10))
+Generations = [{
+    "vectors":np.array([]),
+    "vectors_error":np.array([]),
+    "vectors_percent":np.array([])
+}]
+cur_generation = 0
+
 
 Secret_key = 'hckY8xfUMEtaX9mfpahSAngELplyFXTNXKyXPwYKlnhMAPsocG'
 
@@ -54,52 +62,69 @@ def get_population_errors(population_vector):
 
 def fitness_population(population_vector):
     population_errors = get_population_errors(population_vector)
-    normal_error = population_errors[:,0] * (2/3) + population_errors[:,1] *(1/3)
+    normal_error = population_errors[:,0] * (1/3) + population_errors[:,1] *(2/3)
     population_inverse = np.reciprocal(normal_error+1)
     new_population = storebest_10(population_vector,population_errors,population_inverse)
-    return population_inverse,new_population
+    return new_population
 
 def storebest_10(p_v,p_e,p_per):
-    global B10_V,B10_E
-    index_p = np.argsort(p_per)
-    p_v = p_v[index_p[::-1]]
-    p_e = p_e[index_p[::-1]]
-    p_per = p_per[index_p[::-1]]
+    global B10_V,B10_E,Generations,cur_generation
+    if cur_generation!=0:
+        percents = np.concatenate((Generations[cur_generation]["vectors_percent"],p_per))
+        vectors =  np.concatenate((Generations[cur_generation]["vectors"],p_v))
+        errors = np.concatenate((Generations[cur_generation]["vectors_error"],p_e))
+    else:
+        percents = p_per
+        vectors = p_v
+        errors = p_e
+    index_p = np.argsort(percents)
+    vectors = vectors[index_p[::-1]]
+    errors = errors[index_p[::-1]]
+    percents = percents[index_p[::-1]]
+    index2 = np.array(np.unique(vectors,axis=0,return_index=True)[1])
+    index2 = np.sort(index2)
+    vectors = vectors[index2]
+    errors = errors[index2]
+    percents = percents[index2]
+    Generations.append({"vectors":np.array([]),
+            "vectors_error":np.array([]),
+            "vectors_percent":np.array([])
+            })
+    cur_generation +=1
+    Generations[cur_generation]["vectors"] = vectors[0:MAX_POPULATION]
+    Generations[cur_generation]["vectors_error"] = errors[0:MAX_POPULATION]
+    Generations[cur_generation]["vectors_percent"] = percents[0:MAX_POPULATION]
 
-    if np.array_equal(B10_V,np.zeros((10,MAX_DEG))):
-        B10_V = p_v[0:10]
-        B10_E = p_e[0:10]
-    else :
-        temp_v = np.concatenate((p_v[0:10],B10_V))
-        temp_errors = np.concatenate((p_e[0:10],B10_E))
-        temp_indexs = np.argsort(temp_errors[:,0])
-        temp_v = temp_v[temp_indexs[::-1]]
-        temp_errors = temp_errors[temp_indexs[::-1]] 
-        B10_V = temp_v[-10:]
-        B10_E = temp_errors[-10:]
-        B10_V = B10_V[::-1]
-        B10_E = B10_E[::-1]
-
-    file = open("my_generations4.txt","a")
-    L = [str(p_v),str(p_e),str(p_per)]
-    file.write("\n<--------------Start--------------->\n")
-    file.write(L[0])
-    file.write("\n")
-    file.write(L[1])
-    file.write("\n")
-    file.write(L[2])
-    file.write("\n<---------------End---------------->\n")
-    file.write("\n<----------Best vectors------->\n")
-    file.write(str(B10_V))
-    file.write("\n")
-    file.write(str(B10_E))
-    file.write("\n<---------End--------->\n")
-    file.close()
-    return p_v
+    B10_V = vectors[0:10]
+    B10_E = errors[0:10]
+    name = "./output4/generations"+str(cur_generation)
+    with open(name+".json",'w+') as file:
+        temp = {"vectors":Generations[cur_generation]['vectors'].tolist(),
+            "vectors_error":Generations[cur_generation]['vectors_error'].tolist(),
+            "vectors_percent":Generations[cur_generation]['vectors_percent'].tolist()
+            }
+        json.dump(temp, file)
+        file.close()
+    with open(name+".txt",'w') as file:
+        L = [str(vectors),str(errors),str(percents)]
+        file.write("\n<--------------Start--------------->\n")
+        file.write(L[0])
+        file.write("\n")
+        file.write(L[1])
+        file.write("\n")
+        file.write(L[2])
+        file.write("\n<---------------End---------------->\n")
+        file.write("\n<----------Best vectors------->\n")
+        file.write(str(B10_V))
+        file.write("\n")
+        file.write(str(B10_E))
+        file.write("\n<---------End--------->\n")
+        file.close()
+    return Generations[cur_generation]["vectors"]
 
 def cross_over(parents):
     children = np.array(parents)
-    no_of_places = np.random.randint(low=1,high=parents.shape[1],size=1)
+    no_of_places = np.random.randint(low=3,high=MAX_DEG,size=1)
     selected_places = np.random.choice(parents.shape[1],no_of_places,replace=False)
     for i in selected_places:
         temp = children[0][i]
@@ -108,33 +133,41 @@ def cross_over(parents):
     return children
 
 def mutation(p_vector):
-    return list()
+    num = np.random.randint(low=0,high=100,size=p_vector.shape[0])
+    for i in range(0,num.shape[0]):
+        if num[i]<30:
+            #print(cur_generation,i)
+            place = np.random.randint(low=0,high=MAX_DEG,size=1)
+            #print(place)
+            num_muiltple = uniform(-2,2)
+            temp = p_vector[i][place] *num_muiltple
+            if temp<-10 or temp>10 or temp==0:
+                temp = uniform(-10,10)
+            p_vector[i][place] =temp
+    return p_vector
 
-def get_new_generation(population,p_inverse):
-    half_num = int(population.shape[0]/2)
-    new_genaration = np.zeros((half_num,population.shape[1]))
-    survived_population = population[0:half_num]
+def get_new_generation():
+    global Generations
+    population = np.array(Generations[cur_generation]["vectors"])
+    p_inverse = np.array(Generations[cur_generation]["vectors_percent"])
+    half_num = int(MAX_POPULATION/2)
+    new_genaration = np.zeros((half_num,MAX_DEG))
+    survived_population = np.array(population[0:half_num])
     relative_probabilty = p_inverse[0:half_num]
     relative_probabilty = relative_probabilty / np.sum(relative_probabilty)
     temp = list()
-    for i in range(int(half_num/2)):
+    x = int(half_num/2)
+    while(x!=0):
+        x-=1
         parents_index = np.random.choice(survived_population.shape[0],2,replace=False,p =relative_probabilty)
-        check=0
-        for j in temp:
-            if (j==parents_index).all():
-                i -=1
-                check=1
-                break
-        if check==1:
-            continue
+        parents_index = np.sort(parents_index)
         temp.append(parents_index)
         children = cross_over(survived_population[parents_index])
-        new_genaration[i*2] = children[0]
-        new_genaration[i*2+1] = children[1]
-    #new_genaration = mutation(new_genaration)
-    population[half_num:] = new_genaration
+        new_genaration[x*2] = children[0]
+        new_genaration[x*2+1] = children[1]
+    new_genaration = mutation(new_genaration)
     
-    return population
+    return new_genaration
 
 
 
@@ -143,15 +176,16 @@ def get_new_generation(population,p_inverse):
 # Replace 'SECRET_KEY' with your team's secret key (Will be sent over email)
 if __name__ == "__main__":
     initial_vector = get_overfit_vector(Secret_key)
-    population = genarate_population(initial_vector,20)
+    population = genarate_population(initial_vector,MAX_POPULATION)
     #print(get_population_errors(population))
     new_p = population
-    for _ in range(0,2):
-        p_inv,p_newv = fitness_population(new_p)
-        print(p_inv)
-        new_p = get_new_generation(p_newv,p_inv)
-    p_inv,p_newv = fitness_population(new_p)
-    print(B10_V)
-    print(B10_E)
-    print(B10_V.shape)
-    #submit(Secret_key,list(B10_V[0]))
+    old_p = np.array([])
+    for _ in range(0,30):
+        old_p = fitness_population(new_p)
+        #print(old_p)
+        new_p = get_new_generation()
+    p_newv = fitness_population(new_p)
+    # print(B10_V)
+    # print(B10_E)
+    # print(B10_V.shape)
+    submit(Secret_key,list(B10_V[0]))
